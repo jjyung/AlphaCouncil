153fefc8cdde739d95efc30268b79ec2e7ab26a1
Samson Zheng <samsonzheng@cathayholdings.com.tw>
2026-04-16 16:47:38 +0800

    feat: add chip analyst and chip data caching

--- alpha_council/analysts/__init__.py
+++ alpha_council/analysts/__init__.py
@@ -2,10 +2,12 @@ from alpha_council.analysts.technical_analyst import technical_analyst
 from alpha_council.analysts.news_analyst import news_analyst
 from alpha_council.analysts.psychology_analyst import psychology_analyst
 from alpha_council.analysts.fundamental_analyst import fundamental_analyst
+from alpha_council.analysts.chip_analyst import chip_analyst
 
 __all__ = [
     "technical_analyst",
     "news_analyst",
     "psychology_analyst",
     "fundamental_analyst",
+    "chip_analyst",
 ]
